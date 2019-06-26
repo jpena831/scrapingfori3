275e33183418c834bb8b49d6c7765f0ac4d6ebda
Joslenne <joslennepena@gmail.com>
2019-06-26 17:43:11 -0400

    Update testtweepy.py

--- testtweepy.py
+++ testtweepy.py
@@ -3,8 +3,8 @@ import csv
 import sys
 from urllib.request import urlopen
 
-auth = tweepy.OAuthHandler("YwjEECDNFjGNc44JwGz3d83YM", "rsSUna8KABra0ZeJqnmarMJ4zM5eVNZHt4qQsbAcc2wlckIFe2")
-auth.set_access_token("1120468466-I1yMKwzeqtHiGwsxwNSkhL1lM0iA49NwOvArQGc", "R4tNgI2EdOpFH1PT7OpV6cUh9nksKCfnKaDwWao4V1PxB")
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
 
 api = tweepy.API(auth)
 
