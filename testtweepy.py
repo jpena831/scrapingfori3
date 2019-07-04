fe89cec354e7a30ece4a9df13c1e95cabbf92a09
Joslenne <joslennepena@gmail.com>
2019-07-04 11:58:43 -0400

    Update testtweepy.py

--- testtweepy.py
+++ testtweepy.py
@@ -3,11 +3,13 @@ import csv
 import sys
 from urllib.request import urlopen
 
+#please add your Twitter API credentials below
 auth = tweepy.OAuthHandler("", "")
 auth.set_access_token("", "")
 
 api = tweepy.API(auth)
 
+#this will simply print tweets in your terminal or prompt window based on your account profile timeline
 public_tweets = api.home_timeline()
 # for tweet in public_tweets:
 #   print (tweet.text)
